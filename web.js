9d28b36c64566e0282dcdb2901edf5b6842c01a8
Joao Almeida <joao.diogo.almeida@gmail.com>
2013-07-09 20:04:34 +0000

    swe ase enter the commit message for your changes. Lines starting

--- web.js
+++ web.js
@@ -1,12 +1,17 @@
+#!/usr/bin/env node
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
+var uf = fs.readFileSync('./index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(uf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
